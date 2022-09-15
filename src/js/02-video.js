f50ab0c929fbcd90049f37f2e884b285c9fcd5b2
Oleksandr Kostan <kostan.law@gmail.com>
2022-09-15 23:06:47 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const onPlay = function (event) {
+  localStorage.setItem('videoplayer-current-time', event.seconds);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+if (localStorage.getItem('videoplayer-current-time')) {
+  const currenTime = JSON.parse(
+    localStorage.getItem('videoplayer-current-time')
+  );
+  player.setCurrentTime(currenTime);
+}
